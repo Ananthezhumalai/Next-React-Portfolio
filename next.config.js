312b61517670887d5fea27b1cf259623c09421d4
Manichinu <manikumar31520@gmail.com>
2025-06-24 08:57:55 +0530

    deployed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
- 
-module.exports = {
+ const { withNetlify } = require('@netlify/next');
+module.exports = withNetlify({
   output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -24,4 +24,4 @@ module.exports = {
       },
     ],
   },
-}
+})
